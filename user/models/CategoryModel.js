639581ceff07dda028e29e2102ba1329ec5a59ae
namnguyenit <97675663+namnguyenit@users.noreply.github.com>
2025-06-12 14:11:50 +0700

    update category model

--- user/models/CategoryModel.js
+++ user/models/CategoryModel.js
@@ -1,9 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const categorySchema = new Schema({
-    productID: { type: Schema.Types.ObjectId, ref: 'Product' },
-    name: { type: String, required: true }
+const detailList = new Schema({
+  size: String,
+  price: Number,
+  stock: Number,
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+const productSchema = new Schema({
+  name: String,
+  description: String,
+  type: String,
+  detail: [detailList],
+  image: {
+    imageName: String,
+    imageType: String,
+    imageData: Buffer,
+  },
+});
+
+const categorySchema = new Schema({
+  name: { type: String, required: true },
+  image: {
+    imageName: String,
+    imageType: String,
+    imageData: Buffer,
+  },
+  products: [productSchema],
+});
+
+module.exports = mongoose.model("Category", categorySchema);
