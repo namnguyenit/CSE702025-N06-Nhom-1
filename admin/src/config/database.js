47852f81074565d70f50b47bd86b1a432c6667d4
namnguyenit <97675663+namnguyenit@users.noreply.github.com>
2025-06-18 23:52:51 +0700

    fix lỗi hệ thống database phân tán

--- admin/src/config/database.js
+++ admin/src/config/database.js
@@ -5,6 +5,8 @@ module.exports = {
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASS,
   database: process.env.DB_NAME || "webbtl",
+  // database: process.env.DB_NAME || "web",
   // Thêm biến cluster URI để ưu tiên dùng cho cluster
+  // clusterUri: process.env.MONGODB_URI || "mongodb://localhost:27017/web"
   clusterUri: process.env.MONGODB_URI || "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/webbtl?replicaSet=rs0"
 };
